d78fb8683d93ae48bc44e7ea95eb0b8d3d6e3ea1
Jonathan Sick <jsick@lsst.org>
2017-01-30 23:18:23 -0700

    Add templates and rendering functions
    The proof-of-concept jinja2 templates and their rendering functions.
    Rendering features:
    
    - Bin and sort editions in render_edition() into releases, recent
      development editions and stale development editions (like how github
      presents branches)
    - Hydrate data loaded from the Keeper API with datetime objects and
      timedelta objects that are useful for sorting and filtering.
    
    Template features:
    
    - base template for html5 structure, borrowing from html5bp
      https://html5boilerplate.com
    - Use lists of build/edition slugs as a proof-of-concept at this stage.
    - Templates for individual build or edition items in those lists are
      factored into _items.jinja2 partials that are included in the parent
      template.
    
      Individual editions or builds are "items" in the dashboards. Since
      this pattern is used in both build and edition dashboards (and re-used
      in different time bins of editions as well), it makes sense to
      refactor the definition of an item's template. Hence the "_item.jinja"
      template fragment. The "_" is meant to signify that this is a fragment
      meanted to be "included" in a template, and will not render itself
      into a full HTML document.

---
+++ app/dashboard/render.py
@@ -0,0 +1,102 @@
+"""Template rendering functions."""
+
+import os
+import datetime
+
+import jinja2
+
+
+def render_edition_dashboard(product_data, edition_data):
+    """Render the edition template with data."""
+    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_dir),
+        autoescape=jinja2.select_autoescape(['html'])
+    )
+
+    # hydrate the datasets with datetime objects
+    _insert_datetime(edition_data)
+    _insert_age(edition_data)
+
+    # The main edition is always a release; label it as 'Current' for
+    # template presentation.
+    # More work should be done on how LTD designates releases.
+    releases = [edition_data['main']]
+    releases[0]['alt_title'] = 'Current'
+    release_slugs = [r['slug'] for r in releases]
+
+    # Extract recently-updated editions
+    # Releases are never included in the development lists
+    recent_threshold = datetime.timedelta(days=7)
+    recents = []
+    stales = []
+    for _, edition in edition_data.items():
+        if edition['slug'] not in release_slugs:
+            if edition['age'] <= recent_threshold:
+                recents.append(edition)
+            else:
+                stales.append(edition)
+
+    # Sort editions youngest to oldest
+    releases.sort(key=lambda x: x['age'])
+    recents.sort(key=lambda x: x['age'])
+    stales.sort(key=lambda x: x['age'])
+
+    template = env.get_template('edition_dashboard.jinja')
+    rendered_page = template.render(
+        product=product_data,
+        releases=releases,
+        recents=recents,
+        stales=stales)
+    return rendered_page
+
+
+def render_build_dashboard(product_data, build_data):
+    """Render the builds template with data."""
+    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_dir),
+        autoescape=jinja2.select_autoescape(['html'])
+    )
+
+    # hydrate the datasets with datetime objects
+    _insert_datetime(build_data, datetime_str_key='date_created')
+    _insert_age(build_data, datetime_str_key='date_created')
+
+    builds = [b for _, b in build_data.items()]
+    builds.sort(key=lambda x: x['age'])
+    template = env.get_template('build_dashboard.jinja')
+    rendered_page = template.render(
+        product=product_data,
+        builds=builds)
+    return rendered_page
+
+
+def _insert_datetime(dataset,
+                     datetime_str_key='date_rebuilt',
+                     datetime_key='datetime_rebuilt'):
+    """Inserts a field into every item of an editions
+    or build dataset with a `datetime.datetime` instance corresponding to a
+    ISO datetime string from the Keeper API.
+    """
+    for k, d in dataset.items():
+        d[datetime_key] = _parse_keeper_datetime(d[datetime_str_key])
+    return d
+
+
+def _insert_age(dataset,
+                datetime_str_key='date_rebuilt',
+                age_key='age'):
+    """Inserts the age (from now) of a build into every item of an edition
+    or build dataset.
+
+    Age is persisted as a `datetime.timedelta` instance.
+    """
+    for k, d in dataset.items():
+        dt = _parse_keeper_datetime(d[datetime_str_key])
+        d[age_key] = datetime.datetime.now() - dt
+    return d
+
+
+def _parse_keeper_datetime(date_string):
+    return datetime.datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
