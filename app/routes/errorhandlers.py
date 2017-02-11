e3f141b5c2aecb44e2fbd64d7a4a4e314c86520d
Jonathan Sick <jsick@lsst.org>
2017-02-11 13:35:21 -0700

    Add errorhandlers to API blueprint

--- app/routes/errorhandlers.py
+++ app/routes/errorhandlers.py
@@ -6,8 +6,13 @@ than the default HMTL header response.
 """
 
 from flask import jsonify
+from structlog import get_logger
 from ..exceptions import ValidationError
 from . import api
+
+
+__all__ = ['bad_request', 'not_found', 'method_not_supported',
+           'internal_server_error']
 
 
 @api.errorhandler(ValidationError)
@@ -15,6 +20,8 @@ def bad_request(e):
     """Handler for ValidationError exceptions."""
     response = jsonify({'status': 400, 'error': 'bad request',
                         'message': e.args[0]})
+    logger = get_logger()
+    logger.warn(e.args[0], status=400)
     response.status_code = 400
     return response
 
@@ -24,6 +31,8 @@ def not_found(e):
     """App-wide handler for HTTP 404 errors."""
     response = jsonify({'status': 404, 'error': 'not found',
                         'message': 'invalid resource URI'})
+    logger = get_logger()
+    logger.warn('not found', status=404)
     response.status_code = 404
     return response
 
@@ -33,6 +42,8 @@ def method_not_supported(e):
     """Handler for HTTP 405 exceptions."""
     response = jsonify({'status': 405, 'error': 'method not supported',
                         'message': 'the method is not supported'})
+    logger = get_logger()
+    logger.warn(e.args[0], status=405)
     response.status_code = 405
     return response
 
@@ -42,5 +53,7 @@ def internal_server_error(e):
     """App-wide handler for HTTP 500 errors."""
     response = jsonify({'status': 500, 'error': 'internal server error',
                         'message': e.args[0]})
+    logger = get_logger()
+    logger.error(e.args[0], status=500)
     response.status_code = 500
     return response
