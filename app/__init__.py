d3d989c3f385e74b4a5c6caae1a87b75abc86868
Jonathan Sick <jsick@lsst.org>
2017-02-23 00:16:47 -0700

    Tag v0.1.0

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from . import dashboard  # noqa: F401
 
 
 # Application version; should match Git tags and docker tags
-__version__ = '0.1.0-rc.1'
+__version__ = '0.1.0'
 
 
 def create_app(profile='production'):
