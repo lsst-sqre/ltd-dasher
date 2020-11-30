c79f9a5fdbad410751701f7198331fc71e2fdae9
Jonathan Sick <jsick@lsst.org>
2020-11-30 14:50:48 -0500

    Create 0.1.8 release

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from . import dashboard  # noqa: F401
 
 
 # Application version; should match Git tags and docker tags
-__version__ = '0.1.6'
+__version__ = '0.1.8'
 
 
 def create_app(profile='production'):
